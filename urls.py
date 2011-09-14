fe1d5cc24cc96be22531ddfc1547e64a4bc774c8
Dominic Rodger <dominicrodger+github@gmail.com>
2011-09-14 17:36:17 +0100

    Moving the issue views into an 'issues' folder, so that I can then add a list of issues in the root of the new folder.

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from magazine.views import CurrentIssueListView, IssueView, ArticleView, AuthorD
 
 urlpatterns = patterns('',
     url(r'^$', CurrentIssueListView.as_view(), name='magazine_index'),
-    url(r'^(?P<number>([0-9]+))/$', IssueView.as_view(), name='issue_detail'),
-    url(r'^(?P<number>([0-9]+))/(?P<pk>([0-9]+))/$', ArticleView.as_view(), name='article_detail'),
+    url(r'^issues/(?P<number>([0-9]+))/$', IssueView.as_view(), name='issue_detail'),
+    url(r'^issues/(?P<number>([0-9]+))/(?P<pk>([0-9]+))/$', ArticleView.as_view(), name='article_detail'),
     url(r'^authors/(?P<pk>([0-9]+))/$', AuthorDetailView.as_view(), name='author_detail'),
 )
