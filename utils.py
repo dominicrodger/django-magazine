d7bc9b5863c5777514c67f7da4218c1c97667c3a
Dominic Rodger <dominicrodger+github@gmail.com>
2011-09-24 21:21:55 +0100

    Closes #8 - the content of <style>...</style> is now entirely stripped.

--- utils.py
+++ utils.py
@@ -1,10 +1,16 @@
 import bleach
+from lxml.html.clean import Cleaner
 
 allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2',]
 allowed_attributes = bleach.ALLOWED_ATTRIBUTES.copy()
 allowed_attributes['a'] = bleach.ALLOWED_ATTRIBUTES['a'] + ['name']
 
 def clean_word_text(text):
+    # The only thing I need Cleaner for is to clear out the contents of
+    # <style>...</style> tags
+    cleaner = Cleaner(style = True)
+    text = cleaner.clean_html(text)
+
     text = bleach.clean(text, tags = allowed_tags, strip = True, attributes = allowed_attributes)
 
     return text
