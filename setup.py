79fb33d9e77ec1c0c2fba9e572706bf6512a46d4
Dominic Rodger <dominicrodger+github@gmail.com>
2011-09-26 15:54:09 +0100

    Initial attempt at making requirements.txt and setup.py work.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='django-magazine',
+    version='0.1.0',
+    description='A Django app for managing magazine articles, issues and authors.',
+    long_description=open('README.md').read(),
+    author='Dominic Rodger',
+    author_email='internet@dominicrodger.com',
+    url='http://github.com/dominicrodger/django-magazine',
+    license='BSD',
+    packages=['django-magazine'],
+    include_package_data=True,
+    package_data={'': ['README.md']},
+    zip_safe=False,
+    install_requires=['Django', 'South', 'bleach', 'django-tinymce', 'html5lib',]
+)
