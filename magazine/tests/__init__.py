c62d4352ae275bf3bcede49c5c0bbe5727951a0b
Dominic Rodger <dominicrodger+github@gmail.com>
2011-10-08 08:34:40 +0100

    Separating out tests for Issues.

--- magazine/tests/__init__.py
+++ magazine/tests/__init__.py
@@ -1,2 +1,3 @@
 from magazine.tests.tests import *
 from magazine.tests.authors import AuthorTestCase
+from magazine.tests.issues import IssueTestCase
---
+++ magazine/tests/issues.py
@@ -0,0 +1,65 @@
+from datetime import date, timedelta
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from magazine.models import Issue, subtract_n_months
+
+class IssueTestCase(TestCase):
+    fixtures = ['test_issues.json',]
+
+    def setUp(self):
+        self.issue_1 = Issue.objects.get(pk = 1)
+        self.issue_2 = Issue.objects.get(pk = 2)
+        self.issue_3 = Issue.objects.get(pk = 3)
+
+    def testUnicode(self):
+        self.assertEqual(self.issue_1.__unicode__(), u'Issue 1')
+        self.assertEqual(self.issue_2.__unicode__(), u'Issue 3')
+
+    def testMonthYear(self):
+        self.assertEqual(self.issue_1.month_year(), u'January 2010')
+        self.assertEqual(self.issue_2.month_year(), u'April 2010')
+
+    def testDefaultPublished(self):
+        self.assertEqual(self.issue_1.published, True)
+        self.assertEqual(self.issue_2.published, True)
+
+    def testCurrentIssue(self):
+        self.assertEqual(Issue.current_issue(), self.issue_2)
+        self.issue_2.published = False
+        self.issue_2.save()
+
+        self.assertEqual(Issue.current_issue(), self.issue_1)
+
+        self.issue_2.published = True
+        self.issue_2.save()
+
+    def testGetURL(self):
+        self.assertEqual(self.issue_1.get_absolute_url(), reverse('magazine_issue_detail', args=[self.issue_1.number,]))
+        self.assertEqual(self.issue_2.get_absolute_url(), reverse('magazine_issue_detail', args=[self.issue_2.number,]))
+
+    def testFirstOfMonth(self):
+        issue = Issue.objects.create(number = 4, issue_date = date(2010,4,30))
+        issue_4 = Issue.objects.get(pk = issue.pk)
+        self.assertEqual(issue.issue_date, date(2010,4,1))
+
+    def testIssueEmbargoed(self):
+        self.assertFalse(self.issue_1.embargoed())
+        self.assertFalse(self.issue_2.embargoed())
+        self.assertTrue(self.issue_3.embargoed())
+
+        start_of_month = date.today().replace(day = 1)
+
+        if int(getattr(settings, 'MAGAZINE_EMBARGO_TIME_IN_MONTHS', 2)) > 0:
+            issue = Issue.objects.create(number = 4, issue_date = start_of_month - timedelta(days = 20))
+            issue_4 = Issue.objects.get(pk = issue.pk)
+            self.assertTrue(issue_4.embargoed())
+
+            issue_4.issue_date = subtract_n_months(start_of_month, int(getattr(settings, 'MAGAZINE_EMBARGO_TIME_IN_MONTHS', 2)) - 1)
+            issue_4.save()
+            self.assertTrue(issue_4.embargoed())
+
+            issue_4.issue_date = subtract_n_months(start_of_month, int(getattr(settings, 'MAGAZINE_EMBARGO_TIME_IN_MONTHS', 2)))
+            issue_4.save()
+            self.assertFalse(issue_4.embargoed())
+
