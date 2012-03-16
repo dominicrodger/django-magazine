2725542b56ec0e07be41ba648cf6e57b42a6da8e
Dominic Rodger <dominicrodger+github@gmail.com>
2012-03-16 19:14:45 +0000

    Show newest articles first in the admin.

--- magazine/admin.py
+++ magazine/admin.py
@@ -25,6 +25,7 @@ class ArticleAdmin(AdminImageMixin, admin.ModelAdmin):
     readonly_fields = ('hits',)
     filter_horizontal = ('authors',)
     exclude = ('cleaned_text',)
+    ordering = ('issue',)
 
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name in ('text',):
