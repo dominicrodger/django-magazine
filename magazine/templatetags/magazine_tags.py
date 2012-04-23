b2c2e395244f9f9cd3c0b66f83c22cacb9243e56
Dominic Rodger <dominicrodger+github@gmail.com>
2012-04-23 18:31:31 +0100

    Fixing breaking change cf07b27772.

--- magazine/templatetags/magazine_tags.py
+++ magazine/templatetags/magazine_tags.py
@@ -38,12 +38,12 @@ def magazine_authors(object):
         return render_to_string('magazine/_individual_author.html',
                                 {'author': authors[0]})
     else:
+        final_author = render_to_string('magazine/_individual_author.html',
+                                        {'author': authors[-1]})
         first_bit = ', '.join(
             [render_to_string('magazine/_individual_author.html',
                               {'author': author}) for author in authors[0:-1]])
-        second_bit = ' and ' +
-        render_to_string('magazine/_individual_author.html',
-                         {'author': authors[-1]})
+        second_bit = ' and ' + final_author
         result = first_bit + second_bit
     return mark_safe(result)
 
