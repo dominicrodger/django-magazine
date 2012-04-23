42e50cab16fc6d4a6dfaf613bd721732e12a9ae1
Dominic Rodger <dominicrodger+github@gmail.com>
2012-04-23 22:39:21 +0100

    More PEP8.

--- magazine/tests/utils.py
+++ magazine/tests/utils.py
@@ -2,20 +2,27 @@ from datetime import date
 from django.test import TestCase
 from magazine.models import subtract_n_months
 
+
 class SubtractNMonthsTestCase(TestCase):
     def testWithDateAtMiddleOfYear(self):
-        self.assertEqual(subtract_n_months(date(2010, 4, 7), 2), date(2010, 2, 7))
+        self.assertEqual(subtract_n_months(date(2010, 4, 7), 2),
+                         date(2010, 2, 7))
 
     def testWithDateAtEndOfMonth(self):
         # September only has 30 days
-        self.assertEqual(subtract_n_months(date(2010, 10, 31), 1), date(2010, 9, 30))
+        self.assertEqual(subtract_n_months(date(2010, 10, 31), 1),
+                         date(2010, 9, 30))
 
     def testWithDateAtBeginningOfYear(self):
-        self.assertEqual(subtract_n_months(date(2010, 1, 31), 1), date(2009, 12, 31))
+        self.assertEqual(subtract_n_months(date(2010, 1, 31), 1),
+                         date(2009, 12, 31))
 
     def testWithDateAtBeginningOfYearAtEndOfMonth(self):
-        self.assertEqual(subtract_n_months(date(2010, 3, 31), 4), date(2009, 11, 30))
+        self.assertEqual(subtract_n_months(date(2010, 3, 31), 4),
+                         date(2009, 11, 30))
 
     def testWithMoreThan12Months(self):
-        self.assertEqual(subtract_n_months(date(2010, 3, 31), 25), date(2008, 2, 29))
-        self.assertEqual(subtract_n_months(date(2010, 3, 31), 37), date(2007, 2, 28))
+        self.assertEqual(subtract_n_months(date(2010, 3, 31), 25),
+                         date(2008, 2, 29))
+        self.assertEqual(subtract_n_months(date(2010, 3, 31), 37),
+                         date(2007, 2, 28))
