33fae940883737cc9c4c71ebf0aa342996777236
Dominic Rodger <dominicrodger+github@gmail.com>
2012-04-26 08:19:14 +0100

    Don't repeatedly calculate the same URLs in these tests.

--- magazine/tests/filters.py
+++ magazine/tests/filters.py
@@ -38,8 +38,12 @@ class MagazineTagsTestCase(TestCase):
 
     def setUp(self):
         self.paul = Author.objects.get(pk=1)
+        self.paul_url = self.paul.get_absolute_url()
         self.dom = Author.objects.get(pk=2)
+        self.dom_url = self.dom.get_absolute_url()
         self.bugs = Author.objects.get(pk=3)
+        self.bugs_url = reverse('magazine_author_detail',
+                                args=[self.bugs.pk, ])
 
     def testAuthorTemplate(self):
         class AuthorHolder(object):
@@ -57,26 +61,23 @@ class MagazineTagsTestCase(TestCase):
         result = t.render(Context({'object': AuthorHolder(2, [self.paul, ])}))
         self.assertEqual(striptags(result),
                          'Paul Beasley-Murray')
-        self.assertNotEqual(result.find(self.paul.get_absolute_url()), -1)
-        self.assertEqual(result.find(self.dom.get_absolute_url()), -1)
-        self.assertEqual(result.find(reverse('magazine_author_detail',
-                                             args=[self.bugs.pk, ])), -1)
+        self.assertNotEqual(result.find(self.paul_url), -1)
+        self.assertEqual(result.find(self.dom_url), -1)
+        self.assertEqual(result.find(self.bugs_url), -1)
 
         paul_and_dom = AuthorHolder(3, [self.paul, self.dom, ])
         result = t.render(Context({'object': paul_and_dom}))
         self.assertEqual(striptags(result),
                          'Paul Beasley-Murray and Dominic Rodger')
-        self.assertNotEqual(result.find(self.paul.get_absolute_url()), -1)
-        self.assertNotEqual(result.find(self.dom.get_absolute_url()), -1)
-        self.assertEqual(result.find(reverse('magazine_author_detail',
-                                             args=[self.bugs.pk, ])), -1)
+        self.assertNotEqual(result.find(self.paul_url), -1)
+        self.assertNotEqual(result.find(self.dom_url), -1)
+        self.assertEqual(result.find(self.bugs_url), -1)
 
         paul_dom_and_bugs = AuthorHolder(4, [self.paul, self.dom, self.bugs, ])
         result = t.render(Context({'object': paul_dom_and_bugs}))
         self.assertEqual(striptags(result),
                          'Paul Beasley-Murray, Dominic Rodger and Bugs')
-        self.assertNotEqual(result.find(self.paul.get_absolute_url()), -1)
-        self.assertNotEqual(result.find(self.dom.get_absolute_url()), -1)
+        self.assertNotEqual(result.find(self.paul_url), -1)
+        self.assertNotEqual(result.find(self.dom_url), -1)
         # Bugs is not indexable, and so should not have a link to his profile
-        self.assertEqual(result.find(reverse('magazine_author_detail',
-                                             args=[self.bugs.pk, ])), -1)
+        self.assertEqual(result.find(self.bugs_url), -1)
